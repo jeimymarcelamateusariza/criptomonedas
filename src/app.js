f097a514d0e1643dd28227d96ffa2a517b80eb82
Jeimy Marcela Mateus Ariza <jeimymateus1@gmail.com>
2025-03-05 11:21:44 -0500

    Consultando Api y mostrando resultados

--- src/app.js
+++ src/app.js
@@ -46,7 +46,6 @@ function selectCriptomonedas(criptomonedas){
 
 function leerValor(e){
     objBusquedad[e.target.name] = e.target.value;
-    console.log(objBusquedad)
 }
 
 function submitFormulario(e){
@@ -58,6 +57,9 @@ function submitFormulario(e){
         mostrarAlerta('*Ambos campos son obligatorios*');
         return
     }
+    //Consultar Api con los resultados
+    consultarAPI();
+
 }
 
 function mostrarAlerta(msg){
@@ -69,6 +71,53 @@ function mostrarAlerta(msg){
         divMensaje.textContent = msg;
         formulario.appendChild(divMensaje);
         
-        
+        setTimeout(() => {
+            divMensaje.remove()
+        }, 3000);
+    }
+}
+
+function consultarAPI(){
+    
+    const {moneda, criptomoneda} = objBusquedad;
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(cotizacion => mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]))
+}
+
+function mostrarCotizacionHTML(cotizacion){
+    limpiarHTML();
+    const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+
+    const precio = document.createElement('P');
+    precio.classList.add('precio');
+    precio.innerHTML = `El precio es <span class="font-semibold">${PRICE}</span>`;
+
+    const precioAlto = document.createElement('P');
+    precioAlto.innerHTML = `El precio más alto del día es <span class="font-semibold">${HIGHDAY}</span>`;
+
+    const precioBajo = document.createElement('P');
+    precioBajo.innerHTML = `El precio más bajo del día es <span class="font-semibold">${LOWDAY}</span>`;
+
+    const cambio = document.createElement('P');
+    cambio.innerHTML = `Variación últimas 24 horas <span class="font-semibold">${CHANGEPCT24HOUR} %</span> `;
+
+    
+    const actualizacion = document.createElement('P');
+    actualizacion.innerHTML = `Última actualización <span class="font-semibold">${LASTUPDATE} </span> `;
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(cambio);
+    resultado.appendChild(actualizacion);
+
+}
+
+function limpiarHTML(){
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild)
     }
 }
